ee08f5d8a7c3e700c8ac43e4b55e8ab15fdb1cf2
e2rd\Edvard <e2aard@gmail.com>
2018-01-03 01:25:33 +0200

    fixed size

--- Julia/Form1.Designer.cs
+++ Julia/Form1.Designer.cs
@@ -35,17 +35,20 @@
             this.inputIm = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.itNum = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.newtonA = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(153, 357);
+            this.button1.Location = new System.Drawing.Point(153, 393);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Julia";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // inputRe
             // 
@@ -54,7 +57,7 @@
             this.inputRe.ScrollBars = System.Windows.Forms.ScrollBars.Both;
             this.inputRe.Size = new System.Drawing.Size(53, 20);
             this.inputRe.TabIndex = 1;
-            this.inputRe.Text = "-0.206";
+            this.inputRe.Text = "-0.7";
             // 
             // label1
             // 
@@ -80,21 +83,21 @@
             this.inputIm.Name = "inputIm";
             this.inputIm.Size = new System.Drawing.Size(53, 20);
             this.inputIm.TabIndex = 3;
-            this.inputIm.Text = "0.27085";
+            this.inputIm.Text = "0.27015";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(246, 357);
+            this.button2.Location = new System.Drawing.Point(319, 320);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 5;
-            this.button2.Text = "Mandelbrot ";
+            this.button2.Text = "Draw ";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(341, 357);
+            this.button3.Location = new System.Drawing.Point(340, 393);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 6;
@@ -102,11 +105,51 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 326);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "It:";
+            // 
+            // itNum
+            // 
+            this.itNum.Location = new System.Drawing.Point(64, 323);
+            this.itNum.Name = "itNum";
+            this.itNum.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.itNum.Size = new System.Drawing.Size(53, 20);
+            this.itNum.TabIndex = 7;
+            this.itNum.Text = "50";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(407, 321);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "a (Neton)";
+            // 
+            // newtonA
+            // 
+            this.newtonA.Location = new System.Drawing.Point(464, 319);
+            this.newtonA.Name = "newtonA";
+            this.newtonA.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.newtonA.Size = new System.Drawing.Size(53, 20);
+            this.newtonA.TabIndex = 9;
+            this.newtonA.Text = "1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(923, 387);
+            this.ClientSize = new System.Drawing.Size(915, 381);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.newtonA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.itNum);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
@@ -130,6 +173,10 @@
         private System.Windows.Forms.TextBox inputIm;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox itNum;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox newtonA;
     }
 }
 
--- Julia/Form1.cs
+++ Julia/Form1.cs
@@ -15,19 +15,12 @@ namespace Julia
     public partial class Form1 : Form
     {
         Graphics g;
-        private const double MIN_X = -2.2;
-        private const double MAX_X = 1;
-        private const double MIN_Y = -1.2;
-        private const double MAX_Y = 1.2;
         private int PIC_SIZE = 300;
-        int maxIterations = 255; //after how much iterations the function should stop
-        int M_ITERSTIONS = 250;
+        int J_ITERATIONS = 50; //after how much iterations the function should stop
+        int M_ITERSTIONS = 50;
         int N_MAX_ITERATIONS = 50;
         Bitmap mBmp, jBmp, nBmp;
-        PictureBox p;
-
-        double cRe, cIm;           //real and imaginary part of the constant c, determinate shape of the Julia Set
-        
+        double cRe, cIm;
         private void button3_Click(object sender, EventArgs e)
         {
             Newton(1,1);
@@ -54,28 +47,38 @@ namespace Julia
             this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Julia(1,1);
-        }
-
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
-            Color color = mBmp.GetPixel(e.X, e.Y);
-            double min = -1.5;
-            double max = 1.5;
+            double min = -2;
+            double max = 2;
             double step = (max - min) / PIC_SIZE;
 
-            cIm = e.X * step + min;
-            cRe = e.Y * step + min;
+            cIm = e.Y * step + min;
+            cRe = e.X * step + min;
 
-            jBmp = Julia(0, 0);
+            Complex myC = new Complex(0, 0);
+
+            try
+            {
+                myC = GetMandebrotValue(e.X, e.Y);
+                mBmp = Mandebrot(0, 0);
+                mBmp.SetPixel(e.X, e.Y, Color.Red);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("OUT OF RANGE");
+            }
+
+            Debug.WriteLine("COMPLEX VALUES {0} {1}", myC.Real, myC.Imaginary);
+
+            inputIm.Text = myC.Imaginary.ToString();
+            inputRe.Text = myC.Real.ToString();
+
+            jBmp = Julia(myC.Real, myC.Imaginary);
             g.DrawImage(jBmp, PIC_SIZE + 10, 0, PIC_SIZE, PIC_SIZE);
 
-            mBmp = Mandebrot(0, 0);
-            mBmp.SetPixel(150, 150, Color.Red);
+            
             g.DrawImage(mBmp, 0, 0, PIC_SIZE, PIC_SIZE);
-            Debug.WriteLine("new COLOR is {0} {1}", cRe, cIm);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -83,14 +86,22 @@ namespace Julia
             g = CreateGraphics();
             g.TranslateTransform(0, 0);
 
+            cIm = Convert.ToDouble(inputIm.Text);
+            cRe = Convert.ToDouble(inputRe.Text);
+            J_ITERATIONS = M_ITERSTIONS = N_MAX_ITERATIONS = Convert.ToInt32(itNum.Text);
+            jBmp = Julia(cRe, cIm);
+            g.DrawImage(jBmp, PIC_SIZE + 10, 0, PIC_SIZE, PIC_SIZE);
+
             mBmp = Mandebrot(0, 0);
+            mBmp.SetPixel(150, 150, Color.Red);
             g.DrawImage(mBmp, 0, 0, PIC_SIZE, PIC_SIZE);
-            
+
+
             nBmp = Newton(0, 0);
             g.DrawImage(nBmp, PIC_SIZE * 2 + 20, 0, PIC_SIZE, PIC_SIZE);
         }
 
-        private Bitmap Julia(int a, int b)
+        private Bitmap Julia(double a, double b)
         {
             Debug.WriteLine("new Julia" + a  + " " + b);
             int Width = PIC_SIZE, Height = PIC_SIZE;
@@ -99,28 +110,27 @@ namespace Julia
             for (int y = 0; y < Height; y++)
                 for (int x = 0; x < Width; x++)
                 {
-                    double pr = GetInitValueX(x, Width);//1.5 * (x - Width / 2) / (0.5 * Width);
+                    double pr = GetInitValueX(x, Width) + 0.25;//1.5 * (x - Width / 2) / (0.5 * Width);
                     double pi = GetInitValueY(y, Height); //(y - Height / 2) / (0.5 * Height);
-                    Complex c = new Complex(cRe, cIm);
+                    Complex c = new Complex(a, b);
 
-                    //i will represent the number of iterations
                     Complex newResult = new Complex(pr, pi);
                     Complex oldResult = newResult;
                     int i;
-                    for (i = 0; i < maxIterations; i++)
+                    for (i = 0; i < J_ITERATIONS; i++)
                     {
-                        //remember value of previous iteration
                         oldResult = newResult;
                         //Complex c = new Complex(-0.7f, 0.27015f);
                         Complex oldResult2 = Complex.Multiply(oldResult, oldResult);//z^2
                         Complex oldResult4 = Complex.Multiply(oldResult2, oldResult2);//z^4
                         Complex subResult = Complex.Subtract(oldResult4, oldResult2);
 
-                        newResult = Complex.Add(oldResult2, c);
+                        newResult = Complex.Add(subResult, c);
 
                         if ((newResult.Real * newResult.Real + newResult.Imaginary * newResult.Imaginary) > 4) break;
                     }
-                    Color color  = Color.FromArgb(255, 255, 255 * (i < maxIterations? 1: 0));
+                    int colValue = i >= J_ITERATIONS ? 255 : 0;
+                    Color color  = Color.FromArgb((i * 2) % 255, (i * 3) % 255, (i * 5) % 255);
                     myBitmap.SetPixel(x, y, color);
                 }
             return myBitmap;
@@ -137,12 +147,8 @@ namespace Julia
                 {
                     double pr = GetInitValueX(x, Width);//1.5 * (x - Width / 2) / (0.5 * Width);
                     double pi = GetInitValueY(y, Height); //(y - Height / 2) / (0.5 * Height);
-
-
-                    //double pr = 1.5 * (x - Width / 2) / (0.5 * Width);
-                    //double pi = 1.5 * (y - Height / 2) / (0.5 * Height);
+                    
                     Complex moveC = new Complex(pr, pi); 
-
                     Complex newResult = new Complex(0, 0);
                     Complex oldResult = new Complex(0, 0);
 
@@ -157,7 +163,7 @@ namespace Julia
                         Complex z4 = Complex.Multiply(z2, z2);//z^4
                         Complex subResult = Complex.Subtract(z4, z2);
                         //newResult = Complex.Add(z2, c);
-                        newResult = Complex.Add(z2, moveC);
+                        newResult = Complex.Add(subResult, moveC);
 
                         if ((newResult.Real * newResult.Real + newResult.Imaginary * newResult.Imaginary) > 4) break;
                     }
@@ -166,6 +172,46 @@ namespace Julia
                 }
             return myBitmap;
         }
+
+        private Complex GetMandebrotValue(int x, int y)
+        {
+            Complex c = new Complex(cRe, cIm);
+            Complex newResult = new Complex(0, 0);
+
+            int Width = PIC_SIZE, Height = PIC_SIZE;
+            Bitmap myBitmap = new Bitmap(Width, Height);
+                {
+                    double pr = GetInitValueX(x, Width);//1.5 * (x - Width / 2) / (0.5 * Width);
+                    double pi = GetInitValueY(y, Height); //(y - Height / 2) / (0.5 * Height);
+
+
+                    //double pr = 1.5 * (x - Width / 2) / (0.5 * Width);
+                    //double pi = 1.5 * (y - Height / 2) / (0.5 * Height);
+                    Complex moveC = new Complex(pr, pi);
+
+                    Complex oldResult = new Complex(0, 0);
+
+                    int i;
+                    //start the iteration process
+                    for (i = 0; i < M_ITERSTIONS; i++)
+                    {
+                        oldResult = newResult;
+
+                        Complex z = oldResult;
+                        Complex z2 = Complex.Multiply(z, z);//z^2
+                        Complex z4 = Complex.Multiply(z2, z2);//z^4
+                        Complex subResult = Complex.Subtract(z4, z2);
+                        //newResult = Complex.Add(z2, c);
+                        newResult = Complex.Add(subResult, moveC);
+
+                        if ((newResult.Real * newResult.Real + newResult.Imaginary * newResult.Imaginary) > 4) break;
+                    }
+                    Color color = Color.FromArgb((i * 2) % 255, (i * 3) % 255, (i * 5) % 255);//use color model conversion to get rainbow palette, make brightness black if maxIterations reached
+                    myBitmap.SetPixel(x, y, color);//draw the pixel
+                }
+            return newResult;
+        }
+
 
         private Bitmap Newton(int a, int bi)// Draw the Mandelbrot set.
         {
@@ -198,7 +244,7 @@ namespace Julia
             Complex root2 = new Complex(-1, 0);
             Complex root3 = new Complex(0, 1);
             Complex root4 = new Complex(0, -1);
-
+            Complex a = new Complex(Convert.ToDouble(newtonA.Text), 0);
             for (int i = 0; i < N_MAX_ITERATIONS; i++)
             {
                 Complex f = Complex.Multiply(z , z);
@@ -210,7 +256,7 @@ namespace Julia
                 fp = Complex.Multiply(fp, z);
                 fp = Complex.Multiply(four, fp);
 
-                z = Complex.Subtract(z, (Complex.Divide(f, fp)));
+                z = Complex.Subtract(z, Complex.Multiply((Complex.Divide(f, fp)), a));
 
                 
                 if (Complex.Subtract(z, root2).Magnitude <= EPSILON) return Color.Red;
